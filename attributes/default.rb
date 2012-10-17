937deb3d927f4be9b2c5d9685e8bf0c1a785ccde
Mariano A. Cortesi <mariano@zauberlabs.com>
2012-10-17 00:01:03 -0300

    Adds attribute to configure pg_hba entries
    Adds a new attribute that lets the user configure
    different entries for the pg_hba file.

--- attributes/default.rb
+++ attributes/default.rb
@@ -101,3 +101,8 @@ else
 end
 
 default['postgresql']['listen_addresses'] = "localhost"
+default['postgresql']['hba_conf'] = [
+  {:type => 'local', :database => 'all', :user => 'all', :method => 'ident'},
+  {:type => 'host' , :database => 'all', :user => 'all', :address => '127.0.0.1/32' , :method => 'md5'},
+  {:type => 'host' , :database => 'all', :user => 'all', :address => '::1/128' , :method => 'md5'}
+]
