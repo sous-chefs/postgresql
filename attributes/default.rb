61aa7f44792fc546f871c6597951a8121a6a0bfa
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-07 09:43:45 -0800

    Enable dynamic configuration for pg_hba.conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -133,3 +133,9 @@ else
   default[:postgresql][:config][:lc_time] = 'en_US.UTF-8'
   default[:postgresql][:config][:default_text_search_config] = 'pg_catalog.english'
 end
+default[:postgresql][:pg_hba] = [
+  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
+  {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+]
