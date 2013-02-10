6fedbcb7c917e81d82704b36f70196903d79ec82
Artiom Lunev <artiom.lunev@gmail.com>
2013-02-10 23:34:44 +0200

    [COOK-2387] Pitti Postgresql PPA no longer supported, moving to PGDG apt

--- files/default/tests/minitest/apt_pgdg_postgresql_test.rb
+++ files/default/tests/minitest/apt_pgdg_postgresql_test.rb
@@ -16,12 +16,12 @@
 
 require File.expand_path('../support/helpers', __FILE__)
 
-describe 'postgresql::ppa_pitti_postgresql' do
+describe 'postgresql::apt_pgdg_postgresql' do
   include Helpers::Postgresql
 
-  it 'creates the Pitti PPA sources.list' do
+  it 'creates the PGDG apt sources.list' do
     skip unless %w{debian}.include?(node['platform_family'])
-    file("/etc/apt/sources.list.d/pitti-postgresql-ppa.list").must_exist
+    file("/etc/apt/sources.list.d/apt.postgresql.org-source.list").must_exist
   end
 
   it 'installs postgresql-client-9.2' do
--- test/kitchen/cookbooks/postgresql_test/recipes/apt_pgdg_postgresql.rb
+++ test/kitchen/cookbooks/postgresql_test/recipes/apt_pgdg_postgresql.rb
@@ -1,10 +1,8 @@
 #
-# Cookbook Name:: postgresql
-# Recipe:: client
+# Cookbook Name:: postgresql_test
+# Recipe:: apt_pgdg_postgresql
 #
-# Author:: Joshua Timberman (<joshua@opscode.com>)
-# Author:: Lamont Granquist (<lamont@opscode.com>)
-# Copyright 2009-2011 Opscode, Inc.
+# Copyright 2012, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +17,8 @@
 # limitations under the License.
 #
 
-if(node['postgresql']['enable_pgdg_apt'])
-  include_recipe 'postgresql::apt_pgdg_postgresql'
-end
+node.set['postgresql']['enable_pgdg_apt'] = true
+node.set['postgresql']['version'] = '9.2'
+node.set['postgresql']['client']['packages'] = %w{postgresql-client-9.2 libpq-dev}
 
-if(node['postgresql']['enable_pgdg_yum'])
-  include_recipe 'postgresql::yum_pgdg_postgresql'
-end
-
-node['postgresql']['client']['packages'].each do |pg_pack|
-
-  package pg_pack
-
-end
+include_recipe "postgresql::default"
