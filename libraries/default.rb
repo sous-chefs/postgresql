e66d901e70c3f1369188d06255b32be9c9b559e0
David Crane <davidc@donorschoose.org>
2013-04-12 23:05:30 -0400

    Handle psql non-zero exitstatus as postgresql down

--- libraries/default.rb
+++ libraries/default.rb
@@ -310,17 +310,23 @@ end
 # Function to execute an SQL statement in the template1 database.
 #   Input: Query could be a single String or an Array of String.
 #   Output: A String with |-separated columns and \n-separated rows.
+#           Note an empty output could mean psql couldn't connect.
 # This is easiest for 1-field (1-row, 1-col) results, otherwise
 # it will be complex to parse the results.
 def execute_sql(query)
   # query could be a String or an Array of String
   statement = query.is_a?(String) ? query : query.join("\n")
   @execute_sql ||= begin
-    cmd = shell_out!("psql -q --tuples-only --no-align -d template1 -f -",
+    cmd = shell_out("psql -q --tuples-only --no-align -d template1 -f -",
           :user => "postgres",
           :input => statement
     )
-    if (!cmd.stderr.empty?)
+    # If psql fails, generally the postgresql service is down.
+    # Instead of aborting chef with a fatal error, let's just
+    # pass these non-zero exitstatus back as empty cmd.stdout.
+    if (cmd.exitstatus() == 0 and !cmd.stderr.empty?)
+      # An SQL failure is still a zero exitstatus, but then the
+      # stderr explains the error, so let's rais that as fatal.
       Chef::Log.fatal("psql failed executing this SQL statement:\n#{statement}")
       Chef::Log.fatal(cmd.stderr)
       raise "SQL ERROR"
