sous-chefs/postgresql

commit d9c16689b33724e17f2ccbd5a74aa8d5799d6c3c

Run initdb for any redhat-derived platform version on first install
This should have been done in the 2012-11-02 commit to "[COOK-1844]
Make service name configurable." That change corrected the initdb
call with the version-specific service name, and it also removed
the comment about the code being not valid for 9.x. It should have
removed the "if node[postgresql][version].to_f < 9.0", too.

That is what I have done, to complete that fix.

recipes/server_redhat.rb
@@ -44,10 +44,8 @@ node['postgresql']['server']['packages'].each do |pg_pack|
 
 end
 
-if node['postgresql']['version'].to_f < 9.0
-  execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
-    not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
-  end
+execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
+  not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
 end
 
 service "postgresql" do

commit 05ff11bc7c92d19859016de27d94dfe5e37d3541

fix suse platform regression from d9c16689

recipes/server_redhat.rb
@@ -44,8 +44,12 @@ node['postgresql']['server']['packages'].each do |pg_pack|
 
 end
 
-execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
-  not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
+unless platform_family?("suse")
+
+  execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
+    not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
+  end
+
 end
 
 service "postgresql" do