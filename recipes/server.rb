e325cb5578ca35704ba7491758d5eec45a9e41ac
Kevin Nuckolls <kevin.nuckolls@gmail.com>
2013-08-07 15:31:07 -0500

    should be delayed so you can supply additional configuration in a wrapper before startup

--- recipes/server.rb
+++ recipes/server.rb
@@ -62,7 +62,7 @@ template "#{node['postgresql']['dir']}/postgresql.conf" do
   owner "postgres"
   group "postgres"
   mode 0600
-  notifies :reload, 'service[postgresql]', :immediately
+  notifies :reload, 'service[postgresql]', :delayed
 end
 
 template "#{node['postgresql']['dir']}/pg_hba.conf" do
@@ -70,7 +70,7 @@ template "#{node['postgresql']['dir']}/pg_hba.conf" do
   owner "postgres"
   group "postgres"
   mode 00600
-  notifies :reload, 'service[postgresql]', :immediately
+  notifies :reload, 'service[postgresql]', :delayed
 end
 
 # NOTE: Consider two facts before modifying "assign-postgres-password":
