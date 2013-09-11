9371c80804d48d498a9ac62fe67be615e9027438
malheiros <malheiros@gmail.com>
2013-09-11 17:06:35 -0300

    [COOK-3597] Implementation of initdb_locale attribute for EL (redhat/centos/etc) distros
    Signed-off-by: malheiros <malheiros@gmail.com>

--- recipes/server_redhat.rb
+++ recipes/server_redhat.rb
@@ -59,8 +59,14 @@ end
 
 unless platform_family?("suse")
 
-  execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
-    not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
+  if node['postgresql']['initdb_locale'] != nil
+    execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb #{node['postgresql']['initdb_locale']}" do
+      not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
+    end
+  else
+    execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
+      not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
+    end
   end
 
 end
