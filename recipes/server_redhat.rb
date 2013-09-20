528277d1efe26502579b287cd859f11876eb5e30
malheiros <malheiros@gmail.com>
2013-09-20 18:00:47 -0300

    When interpolating a nil value, the result is an empty string. So this can actually be shortened and the conditional removed.

--- recipes/server_redhat.rb
+++ recipes/server_redhat.rb
@@ -59,14 +59,8 @@ end
 
 unless platform_family?("suse")
 
-  if node['postgresql']['initdb_locale'] != nil
-    execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb #{node['postgresql']['initdb_locale']}" do
-      not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
-    end
-  else
-    execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb" do
-      not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
-    end
+  execute "/sbin/service #{node['postgresql']['server']['service_name']} initdb #{node['postgresql']['initdb_locale']}" do
+    not_if { ::FileTest.exist?(File.join(node['postgresql']['dir'], "PG_VERSION")) }
   end
 
 end
