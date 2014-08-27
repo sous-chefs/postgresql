a0848f01149bfd8b3fb8b1ebde063e0680ec76ee
Alexey Kharchenko <akharchenko@gmail.com>
2014-08-27 14:11:07 +0400

    adding version support for packages

--- recipes/server_debian.rb
+++ recipes/server_debian.rb
@@ -23,7 +23,7 @@ include_recipe "postgresql::client"
 
 node['postgresql']['server']['packages'].each do |pg_pack|
 
-  package pg_pack
+  versioned_package pg_pack
 
 end
 
--- recipes/server_redhat.rb
+++ recipes/server_redhat.rb
@@ -47,7 +47,7 @@ end
 
 node['postgresql']['server']['packages'].each do |pg_pack|
 
-  package pg_pack
+  versioned_package pg_pack
 
 end
 
