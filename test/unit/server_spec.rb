7892f8267f8a5abce4de9d6d0a83c9e4f4641e48
Ryan Hass <ryan_hass@rapid7.com>
2014-10-30 23:28:50 -0700

    Add ChefSpec for server recipe.

--- test/unit/server_spec.rb
+++ test/unit/server_spec.rb
@@ -24,13 +24,45 @@ describe 'postgresql::server' do
             :platform => platform.to_s,
             :version => version.to_s
           ) do |node|
-          node.set['postgresql']['password']['postgres'] = 'ilikewaffles'
+            node.set['postgresql']['password']['postgres'] = 'ilikewaffles'
+            node.set['postgresql']['config']['ssl_cert_file'] = '/tmp/server.crt'
+            node.set['postgresql']['config']['ssl_key_file'] = '/tmp/server.key'
           end.converge(described_recipe)
         }
 
-        it 'runs no tests' do
-          expect(chef_run)
+        it 'installs the postgresql client packages from the client recipe' do
+          expect(chef_run).to include_recipe('postgresql::client')
+
+          chef_run.node['postgresql']['client']['packages'].each do |pkg|
+            expect(chef_run).to install_package(pkg)
+          end
         end
+
+        # Broken test.
+        #it 'runs the postgresql service' do
+        #  expect(chef_run).to notify('service[postgresql]')
+        #end
+
+        it 'symlinks the ssl_cert_file and ssl_key_file when the postgresql version is < 9.2' do
+          if chef_run.node['postgresql']['version'].to_f < 9.2
+            expect(chef_run).to create_link("#{::File.join(chef_run.node['postgresql']['config']['data_directory'], 'server.crt')}").with(to: '/tmp/server.crt')
+            expect(chef_run).to create_link("#{::File.join(chef_run.node['postgresql']['config']['data_directory'], 'server.key')}").with(to: '/tmp/server.key')
+          else
+            expect(chef_run).not_to create_link("#{::File.join(chef_run.node['postgresql']['config']['data_directory'], 'server.crt')}").with(to: '/tmp/server.crt')
+            expect(chef_run).not_to create_link("#{::File.join(chef_run.node['postgresql']['config']['data_directory'], 'server.key')}").with(to: '/tmp/server.key')
+          end
+        end
+
+        it 'only adds ssl_key_file or ssl_cert_file options to postgresql.conf when postgresql version is >= 9.2' do
+          if chef_run.node['postgresql']['version'].to_f < 9.2
+            expect(chef_run).not_to render_file("#{::File.join(chef_run.node['postgresql']['dir'], 'postgresql.conf')}").with_content(/^ssl_cert_file/)
+            expect(chef_run).not_to render_file("#{::File.join(chef_run.node['postgresql']['dir'], 'postgresql.conf')}").with_content(/^ssl_key_file/)
+          else
+            expect(chef_run).to render_file("#{::File.join(chef_run.node['postgresql']['dir'], 'postgresql.conf')}").with_content(/^ssl_cert_file/)
+            expect(chef_run).to render_file("#{::File.join(chef_run.node['postgresql']['dir'], 'postgresql.conf')}").with_content(/^ssl_key_file/)
+          end
+        end
+
       end
     end
   end
