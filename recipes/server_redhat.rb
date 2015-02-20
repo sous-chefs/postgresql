7dc9ecff7c5f3a9e88c6fe7d62b63b52e779b2a5
Giorgos Saslis <g.saslis@gmail.com>
2015-02-20 13:52:06 +0200

    Bugfix: Fix typo that addresses issue hw-cookbooks#220

--- recipes/server_redhat.rb
+++ recipes/server_redhat.rb
@@ -77,7 +77,7 @@ if platform_family?("fedora") and node['platform_version'].to_i >= 16
     not_if { ::FileTest.exist?(File.join(dir, "PG_VERSION")) }
   end
 
-else !platform_family?("suse") 
+elsif !platform_family?("suse")
 
   execute "/sbin/service #{svc_name} initdb #{initdb_locale}" do
     not_if { ::FileTest.exist?(File.join(dir, "PG_VERSION")) }
