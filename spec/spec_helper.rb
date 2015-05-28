d09f0f9ffa81a627d686fd2e66f206920b31f64c
Cameron Johnston <cameron@rootdown.net>
2015-05-28 10:46:59 -0600

    fix spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,20 @@
-require 'rspec/expectations'
-require 'chefspec'
-require 'chefspec/berkshelf'
+COOKBOOK_RESOLVERS = {
+  'batali' => ['Batali', 'batali/chefspec'],
+  'berkshelf' => ['Berksfile', 'chefspec/berkshelf'],
+  'librarian' => ['Cheffile', 'chefspec/librarian']
+}
 
-# Require all our libraries
-Dir['libraries/*_helper.rb'].each { |f| require File.expand_path(f) }
+require 'chefspec'
+
+if ENV['COOKBOOK_RESOLVER']
+  require COOKBOOK_RESOLVERS[ENV['COOKBOOK_RESOLVER']]
+else
+  resolver_lib = COOKBOOK_RESOLVERS.values.detect do |r_file, _r_lib|
+    File.exist?(File.join(File.dirname(__FILE__), '..', r_file))
+  end
+  fail 'Failed to locate valid cookbook resolver files!' unless resolver_lib
+  puts "Resolving cookbooks from #{resolver_lib.first}"
+  require resolver_lib.last
+end
 
 at_exit { ChefSpec::Coverage.report! }
