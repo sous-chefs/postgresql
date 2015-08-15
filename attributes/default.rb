0563317c53e0ddc10024ec304007f1547a69e3c1
Yavor Nikolov <nikolov.javor@gmail.com>
2015-08-16 00:31:44 +0300

    Add support for postgresql 9.4 for centos 7

--- attributes/default.rb
+++ attributes/default.rb
@@ -239,6 +239,11 @@ default['postgresql']['pgdg']['repo_rpm_url'] = {
       "7" => {
         "x86_64" => "http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-redhat94-9.4-1.noarch.rpm"
       }
+    },
+    "centos" => {
+      "7" => {
+        "x86_64" => "http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-1.noarch.rpm"
+      }
     }
   },
   "9.3" => {
