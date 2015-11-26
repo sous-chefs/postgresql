8f3667e3e7c4c49463373f7c7e9e5ee064ea466b
Aaron Baer <aaron@hw-ops.com>
2015-11-25 17:54:17 -0800

    pgdg repository for postgresql 9.2 have newer certificates than what is default on rhel dist. we have to always make sure ca-certificates is updated before installing

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -35,6 +35,9 @@ rescue LoadError
 
   if node['postgresql']['enable_pgdg_yum']
     repo_rpm_url, repo_rpm_filename, repo_rpm_package = pgdgrepo_rpm_info
+    package "ca-certificates" do
+      action :nothing
+    end.run_action(:upgrade)
     include_recipe "postgresql::yum_pgdg_postgresql"
     resources("remote_file[#{Chef::Config[:file_cache_path]}/#{repo_rpm_filename}]").run_action(:create)
     resources("package[#{repo_rpm_package}]").run_action(:install)
