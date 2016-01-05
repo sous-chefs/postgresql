e6977358d761236f10e1d5359f546e45000291a8
Aaron Baer <aaron@hw-ops.com>
2016-01-04 16:15:09 -0800

    develop version 4.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "helpdesk@heavywater.io"
 license           "Apache 2.0"
 description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.0.0"
+version           "4.0.1"
 recipe            "postgresql", "Includes postgresql::client"
 recipe            "postgresql::ruby", "Installs pg gem for Ruby bindings"
 recipe            "postgresql::client", "Installs postgresql client package(s)"
