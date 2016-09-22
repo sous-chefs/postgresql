af0a1b7f1da6899608d126c1c879c76d0f8aaf38
Gennadiy Nerubayev <gennadiy.nerubayev@schrodinger.com>
2016-09-22 02:05:54 -0400

    Allow usage of a different pg gem version to preserve backwards compatibility with older chef versions.

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -82,6 +82,7 @@ rescue LoadError
   begin
     chef_gem "pg" do
       compile_time true if respond_to?(:compile_time)
+      version node['postgresql']['pg_gem_version'] if defined? node['postgresql']['pg_gem_version']
     end
   rescue Gem::Installer::ExtensionBuildError, Mixlib::ShellOut::ShellCommandFailed => e
     # Are we an omnibus install?
