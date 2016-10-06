9f8b42fbc304f6678be7bed043840921d0b331c6
Tim Smith <tsmith@chef.io>
2016-10-05 17:35:50 -0700

    Remove the cleanup of the super old PPA
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -2,12 +2,7 @@ unless %w(jessie squeeze wheezy sid lucid precise saucy trusty utopic wily xenia
   raise 'Not supported release by PGDG apt repository'
 end
 
-include_recipe 'apt'
-
-file 'remove deprecated Pitti PPA apt repository' do
-  action :delete
-  path '/etc/apt/sources.list.d/pitti-postgresql-ppa'
-end
+include_recipe 'apt::default'
 
 apt_repository 'apt.postgresql.org' do
   uri 'http://apt.postgresql.org/pub/repos/apt'
