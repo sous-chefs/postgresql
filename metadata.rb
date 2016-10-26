396d92f6cebe4ed1079468b082db0b43465de181
Cameron Johnston <cameron@rootdown.net>
2016-10-26 11:12:37 -0600

    bump version to 5.0.1 for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'helpdesk@heavywater.io'
 license           'Apache 2.0'
 description       'Installs and configures postgresql for clients or servers'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '5.0.0'
+version           '5.0.1'
 source_url        'https://github.com/hw-cookbooks/postgresql' if respond_to?(:source_url)
 issues_url        'https://github.com/hw-cookbooks/postgresql/issues' if respond_to?(:issues_url)
 chef_version      '>= 11.0' if respond_to?(:chef_version)
