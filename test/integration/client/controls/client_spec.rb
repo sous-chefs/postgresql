f3ae5a550582b07fd311227fb2f3ed56522d76ed
Rene Martin <rene_martin@intuit.com>
2017-02-14 10:47:35 +0000

    Refactor test attributes into the control files

--- test/integration/client/controls/client_spec.rb
+++ test/integration/client/controls/client_spec.rb
@@ -1,5 +1,53 @@
 # frozen_string_literal: true
-pg_version = attribute('pg_version', default: '9.4', description: 'PostgreSQL installed version by default')
+pg_version = attribute('pg_version', default: nil, description: 'PostgreSQL installed version by default')
+
+if pg_version.nil?
+  # Default values per platform/version
+  case os[:family]
+  when 'redhat'
+    case os[:release]
+    when /(5|6)\.\d+/
+      pg_version = '8.4'
+
+    when /7\.\d+/
+      pg_version = '9.2'
+    end
+
+  when 'fedora'
+    case os[:release]
+    when '25'
+      pg_version = '9.5'
+    end
+
+  when 'debian'
+    case os[:release]
+    when /7\.\d+/
+      pg_version = '9.1'
+
+    when /8\.\d+/
+      pg_version = '9.4'
+
+    # Ubuntu versions
+    when '12.04'
+      pg_version = '9.1'
+
+    when '14.04'
+      pg_version = '9.3'
+
+    when '16.04'
+      pg_version = '9.5'
+    end
+
+  when 'suse'
+    case os[:release]
+    when '42.2'
+      pg_version = '9.4'
+
+    when '13.2'
+      pg_version = '9.3'
+    end
+  end
+end
 
 describe command('/usr/bin/psql --help') do
   its('exit_status') { should eq 0 }
