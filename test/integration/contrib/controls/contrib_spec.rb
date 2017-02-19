741706c2e3b4b93620f4b8661279ab47e0b5c276
Rene Martin <rene_martin@intuit.com>
2017-02-19 09:28:08 +0000

    WIP initial set of tests working in debian based distros

---
+++ test/integration/contrib/controls/contrib_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+include_controls 'server'
+
+sql = postgres_session('postgres', 'iloverandompasswordsbutthiswilldo')
+
+%w(plpgsql xml2).each do |pg_ext|
+  describe sql.query("SELECT 'installed' FROM pg_extension WHERE extname = '#{pg_ext}';", ['template1']) do
+      its(:lines) { should eq ['installed'] }
+  end
+end
