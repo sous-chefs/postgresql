a6d98244fbd569c9c32c6f5e73610c0c149b3dd2
Rene Martin <rene_martin@intuit.com>
2017-02-19 21:24:53 +0000

    Simpler platform comparison

--- test/cookbooks/test/recipes/contrib.rb
+++ test/cookbooks/test/recipes/contrib.rb
@@ -2,6 +2,6 @@
 apt_update 'update' if platform_family?('debian')
 
 node.default['postgresql']['password']['postgres'] = 'iloverandompasswordsbutthiswilldo'
-node.default['postgresql']['contrib']['extensions'] = %w(plpgsql xml2) unless platform_family?('rhel') && Gem::Version.new(node['platform_version']) < Gem::Version.new('7')
+node.default['postgresql']['contrib']['extensions'] = %w(plpgsql xml2) unless platform_family?('rhel') && node['platform_version'].to_i < 7
 
 include_recipe 'postgresql::contrib'
