b87d430f43fe835678cae1cce1276b035e50f1cd
Dan Webb <dan.webb@damacus.io>
2017-07-13 09:34:20 +0100

    Fix foodcritic linting. Ignore FC075 for now

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@
 name              'postgresql'
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs and configures postgresql for clients or servers'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '6.1.1'
