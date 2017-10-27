eecfc6e0c27598b75dad9b9bd5da23baa5b18ed2
Tim Smith <tsmith@chef.io>
2017-10-27 12:57:27 -0700

    Support the Fedora repo
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/repository.rb
+++ resources/repository.rb
@@ -27,14 +27,14 @@ property :apt_gpg_key_uri, String, default: 'https://download.postgresql.org/pub
 action :add do
   case node['platform_family']
 
-  when 'rhel'
+  when 'rhel', 'fedora'
     remote_file "/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-#{new_resource.version}" do
       source new_resource.yum_gpg_key_uri
     end
 
     yum_repository "PostgreSQL #{new_resource.version} $releasever - $basearch" do
       repositoryid "pgdg#{new_resource.version}"
-      baseurl     "https://download.postgresql.org/pub/repos/yum/#{new_resource.version}/redhat/rhel-$releasever-$basearch"
+      baseurl     "https://download.postgresql.org/pub/repos/yum/#{new_resource.version}/#{yum_repo_platform}/#{node['platform_family']}-$releasever-$basearch"
       enabled     new_resource.enable_pgdg
       gpgcheck    true
       gpgkey      "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-#{new_resource.version}"
@@ -42,7 +42,7 @@ action :add do
 
     yum_repository "PostgreSQL #{new_resource.version} $releasever - $basearch - source " do
       repositoryid "pgdg#{new_resource.version}-source"
-      baseurl     "https://download.postgresql.org/pub/repos/yum/srpms/#{new_resource.version}/redhat/rhel-$releasever-$basearch"
+      baseurl     "https://download.postgresql.org/pub/repos/yum/srpms/#{new_resource.version}/#{yum_repo_platform}/#{node['platform_family']}-$releasever-$basearch"
       enabled     new_resource.enable_pgdg_source
       gpgcheck    true
       gpgkey      "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-#{new_resource.version}"
@@ -50,7 +50,7 @@ action :add do
 
     yum_repository "PostgreSQL #{new_resource.version} $releasever - $basearch - updates testing" do
       repositoryid "pgdg#{new_resource.version}-updates-testing"
-      baseurl     "https://download.postgresql.org/pub/repos/yum/testing/#{new_resource.version}/redhat/rhel-$releasever-$basearch"
+      baseurl     "https://download.postgresql.org/pub/repos/yum/testing/#{new_resource.version}/#{yum_repo_platform}/#{node['platform_family']}-$releasever-$basearch"
       enabled     new_resource.enable_pgdg_updates_testing
       gpgcheck    true
       gpgkey      "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-#{new_resource.version}"
@@ -58,7 +58,7 @@ action :add do
 
     yum_repository "PostgreSQL #{new_resource.version} $releasever - $basearch - source - updates testing" do
       repositoryid "pgdg#{new_resource.version}-source-updates-testing"
-      baseurl     "https://download.postgresql.org/pub/repos/yum/srpms/testing/#{new_resource.version}/redhat/rhel-$releasever-$basearch"
+      baseurl     "https://download.postgresql.org/pub/repos/yum/srpms/testing/#{new_resource.version}/#{yum_repo_platform}/#{node['platform_family']}-$releasever-$basearch"
       enabled     new_resource.enable_pgdg_source_updates_testing
       gpgcheck    true
       gpgkey      "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-#{new_resource.version}"
@@ -85,6 +85,13 @@ action :add do
       cache_rebuild true
     end
   else
-    raise "The platform_family #{node['platform_family']} or #{node['platform']} is not supported by the postgresql_repository resource. If you believe this platform can/should be supported by this resource please file and issue or open a pull request at https://github.com/sous-chefs/postgresql"
+    raise "The platform_family '#{node['platform_family']}' or platform '#{node['platform']}' is not supported by the postgresql_repository resource. If you believe this platform can/should be supported by this resource please file and issue or open a pull request at https://github.com/sous-chefs/postgresql"
+  end
+end
+
+action_class do
+  # the postgresql yum repos are for either "redhat" or "fedora". Route things to the right place based on platform_family
+  def yum_repo_platform
+    platform_family?('fedora') ? 'fedora' : 'redhat'
   end
 end
