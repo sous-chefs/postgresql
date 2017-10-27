0bd78836447597f0a818acd66f4fe361c6f3657f
Tim Smith <tsmith@chef.io>
2017-10-27 15:29:43 -0700

    Only test our new resources for now
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/apt-pgdg-client.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-apt_update 'update' if platform_family?('debian')
-
-node.default['postgresql']['enable_pgdg_apt'] = true
-node.default['postgresql']['version'] = '9.4'
-node.default['postgresql']['client']['packages'] = ['postgresql-client-9.4', 'libpq-dev']
-
-include_recipe 'postgresql::default'
--- test/cookbooks/test/recipes/yum-pgdg-client.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-node.default['postgresql']['enable_pgdg_yum'] = true
-node.default['postgresql']['version'] = '9.4'
-node.default['postgresql']['client']['packages'] = 'postgresql94'
-
-include_recipe 'postgresql::default'
