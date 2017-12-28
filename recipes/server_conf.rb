ea66b224316416ce53baedd1d888ff66285d99ed
Dan Webb <dan.webb@damacus.io>
2017-12-28 21:18:09 +0000

    Deprecate all the recipes

--- recipes/server_conf.rb
+++ recipes/server_conf.rb
@@ -1,19 +1,2 @@
 # frozen_string_literal: true
-#
-# Cookbook:: postgresql
-# Recipe:: server
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 Chef::Log.warn('The postgresql::server_conf recipe has been deprecated and will be removed in the next major release of the cookbook. Please use the postgresql_server_conf resource instead')
