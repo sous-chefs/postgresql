ea57218770cc27f42c10063c77eaa2a7cfd4cc80
Tim Smith <tsmith@chef.io>
2017-12-28 10:53:24 -0800

    Fix RHEL inspecs tests for the repos (#474)
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/repo/controls/repo_spec.rb.rb
+++ test/integration/repo/controls/repo_spec.rb.rb
@@ -4,19 +4,24 @@ case os[:family]
 
 when 'redhat'
 
-  describe yum.repo('pgdg10') do
-    it { should exist }
-    it { should_not be_enabled }
-  end
-
-  describe yum.repo('pgdg10-source') do
-    it { should exist }
-    it { should_not be_enabled }
-  end
-
-  describe yum.repo('pgdg10-updates-testing') do
+  describe yum.repo('pgdg9.5') do
     it { should exist }
     it { should be_enabled }
+  end
+
+  describe yum.repo('pgdg9.5-source') do
+    it { should exist }
+    it { should_not be_enabled }
+  end
+
+  describe yum.repo('pgdg9.5-source-updates-testing') do
+    it { should exist }
+    it { should_not be_enabled }
+  end
+
+  describe yum.repo('pgdg9.5-updates-testing') do
+    it { should exist }
+    it { should_not be_enabled }
   end
 
 when 'debian'
