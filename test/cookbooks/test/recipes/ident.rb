0822059f03882403dd328eefd689ffc8d4221f9f
Aaron Kalin <akalin@martinisoftware.com>
2018-04-15 21:33:57 -0500

    Add in ident test recipe

---
+++ test/cookbooks/test/recipes/ident.rb
@@ -0,0 +1,54 @@
+postgresql_server_install 'postgresql' do
+  version '9.6'
+  password '12345'
+  port 5432
+  setup_repo true
+  action [:install, :create]
+end
+
+postgresql_ident 'shef mapping' do
+  mapname 'testmap'
+  system_user 'shef'
+  pg_user 'sous_chef'
+  notifies :reload, 'service[postgresql]'
+end
+
+postgresql_ident 'postgresl mapping' do
+  mapname 'testmap'
+  system_user 'postgres'
+  pg_user 'postgres'
+  notifies :reload, 'service[postgresql]'
+end
+
+postgresql_user 'sous_chef test user' do
+  user 'sous_chef'
+  superuser true
+  password '67890'
+end
+
+postgresql_access 'postgresql host superuser' do
+  access_type 'host'
+  access_db 'all'
+  access_user 'postgres'
+  access_addr '127.0.0.1/32'
+  access_method 'md5'
+  notifies :reload, 'service[postgresql]'
+end
+
+postgresql_access 'shef mapping' do
+  access_type 'local'
+  access_db 'all'
+  access_user 'all'
+  access_method 'peer map=testmap'
+  cookbook 'test'
+  notifies :reload, 'service[postgresql]'
+end
+
+user 'shef'
+
+service 'postgresql' do
+  extend PostgresqlCookbook::Helpers
+  service_name lazy { platform_service_name }
+  supports restart: true, status: true, reload: true
+  action :nothing
+end
