d18af7e0567fdf438932f5b97086b24025cafd16
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2022-11-30 16:31:39 +0000

    Update ident resource to use parser class

--- resources/ident.rb
+++ resources/ident.rb
@@ -23,39 +23,53 @@ property :config_file, String,
 property :source, String,
           default: 'pg_ident.conf.erb'
 
-property :mapname, String,
+property :map_name, String,
+          name_property: true
+
+property :system_username, String,
           required: true
 
-property :system_user, String,
-          required: true
-
-property :pg_user, String,
+property :database_username, String,
           required: true
 
 property :comment, String
 
+load_current_value do |new_resource|
+  current_value_does_not_exist! unless ::File.exist?(new_resource.config_file)
+
+  if ::File.exist?(new_resource.config_file)
+    owner ::Etc.getpwuid(::File.stat(new_resource.config_file).uid).name
+    group ::Etc.getgrgid(::File.stat(new_resource.config_file).gid).name
+    filemode ::File.stat(new_resource.config_file).mode.to_s(8)[-4..-1]
+  end
+
+  ident_file = PostgreSQL::Cookbook::IdentHelpers::PgIdent::PgIdentFile.read(new_resource.config_file)
+
+  current_value_does_not_exist! unless ident_file.entry?(new_resource.map_name)
+
+  entry = ident_file.entry(new_resource.map_name)
+  %i(map_name system_username database_username).each { |p| send(p, entry.send(p)) }
+end
+
 action_class do
-  include PostgreSQL::Cookbook::Helpers
+  include PostgreSQL::Cookbook::IdentHelpers::PgIdentTemplate
 end
 
 action :create do
-  ident_resource = new_resource
-  with_run_context :root do
-    edit_resource(:template, new_resource.config_file) do |new_resource|
-      source new_resource.source
-      cookbook new_resource.cookbook
-      owner 'postgres'
-      group 'postgres'
-      mode '0640'
-      variables[:pg_ident] ||= {}
-      variables[:pg_ident][new_resource.name] = {
-        comment: new_resource.comment,
-        mapname: new_resource.mapname,
-        system_user: new_resource.system_user,
-        pg_user: new_resource.pg_user,
-      }
-      action :nothing
-      delayed_action :create
-    end
+  converge_if_changed do
+    config_resource_init
+
+    resource_properties = %i(map_name system_username database_username).map { |p| new_resource.send(p) }.compact
+    entry = PostgreSQL::Cookbook::IdentHelpers::PgIdent::PgIdentFileEntry.new(*resource_properties)
+
+    config_resource.variables[:pg_ident].add(entry)
   end
+end
+
+action :delete do
+  config_resource_init
+
+  converge_by("Remove ident entry with map_name: #{new_resource.map_name}") do
+    config_resource.variables[:pg_ident].remove(new_resource.map_name)
+  end if config_resource.variables[:pg_ident].entry?(new_resource.map_name)
 end
