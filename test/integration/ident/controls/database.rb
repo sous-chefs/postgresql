022cbdd8170f3d4ba7e125d276be3e5e30154014
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2022-12-04 17:05:16 +0000

    Add database inspec test

---
+++ test/integration/ident/controls/database.rb
@@ -0,0 +1,12 @@
+
+control 'test1 database should exist' do
+  impact 1.0
+  desc 'The test1 database should exist'
+
+  postgres_access = postgres_session('postgres', '12345', '127.0.0.1')
+
+  describe postgres_access.query('SELECT * FROM pg_database;') do
+    its('output') { should include 'test1' }
+    its('output') { should_not include 'test2' }
+  end
+end
