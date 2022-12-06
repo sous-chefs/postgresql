82c7ed3c3b068ae778174b13dfb02d515ba33cf0
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2022-12-06 15:41:07 +0000

    Test property update

--- test/cookbooks/test/recipes/access.rb
+++ test/cookbooks/test/recipes/access.rb
@@ -4,17 +4,11 @@ postgresql_install 'postgresql' do
   action %i(install init_server)
 end
 
-postgresql_service 'postgresql' do
-  action %i(enable start)
-end
-
 postgresql_access 'postgresql all all trust' do
   type 'local'
   database 'all'
   user 'all'
   auth_method 'trust'
-
-  notifies :restart, 'postgresql_service[postgresql]', :delayed
 end
 
 postgresql_access 'postgresql host superuser' do
@@ -23,13 +17,15 @@ postgresql_access 'postgresql host superuser' do
   user 'postgres'
   address '127.0.0.1/32'
   auth_method 'md5'
+end
 
-  notifies :restart, 'postgresql_service[postgresql]', :delayed
+postgresql_service 'postgresql' do
+  action %i(enable start)
 end
 
 postgresql_user 'sous_chef' do
   superuser true
-  password '67890'
+  unencrypted_password '67890'
   sensitive false
 end
 
@@ -50,7 +46,7 @@ postgresql_access 'a sous_chef local superuser' do
 end
 
 postgresql_user 'name-with-dash' do
-  password '1234'
+  unencrypted_password '1234'
 end
 
 postgresql_user 'name-with-dash' do
@@ -60,6 +56,6 @@ postgresql_user 'name-with-dash' do
 end
 
 postgresql_user 'dropable-user' do
-  password '1234'
+  unencrypted_password '1234'
   action [:create, :drop]
 end
