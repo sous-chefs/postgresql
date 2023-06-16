cb1f7ac123c9adef559cb1c69789362ea86afad2
b.courtois <b.courtois@criteo.com>
2023-06-16 21:53:59 +0200

    Add test for postgresql_access with multiple auth_options
    Some auth methods require multiple auth options values.
    For instance ldap access has ldapserver, ldapbinddn, etc.
    
    The Regex used to identify the HBA config components needs to accept
    multiple components separated by spaces.
    
    Signed-off-by: b.courtois <b.courtois@criteo.com>

--- test/integration/access/controls/base_access.rb
+++ test/integration/access/controls/base_access.rb
@@ -126,3 +126,17 @@ control 'postgresql-access-auth_options-with-url' do
     its('auth_params') { should cmp 'ldapurl="ldap://ldap.example.net/dc=example,dc=net?uid?sub"' }
   end
 end
+
+control 'postgresql-access-multiple-auth_options' do
+  impact 1.0
+  desc 'This test ensures multiple auth_options may  be specified'
+
+  describe postgres_hba_conf.where { user == 'ldap_options.user' } do
+    its('type') { should cmp 'host' }
+    its('database') { should cmp 'all' }
+    its('user') { should cmp 'ldap_options.user' }
+    its('address') { should cmp '127.0.0.1/32' }
+    its('auth_method') { should cmp 'ldap' }
+    its('auth_params') { should cmp 'ldapbasedn="dc=example, dc=net" ldapsearchattribute=uid ldapserver=ldap.example.net' }
+  end
+end
